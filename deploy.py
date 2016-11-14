277d4dd336a722105328f199920b845dde46049c
jenningsloy318 <jenningsloy318@gmail.com>
2016-11-14 15:04:57 +0800

    fix the regex in find command

--- deploy.py
+++ deploy.py
@@ -117,8 +117,8 @@ def gen_std_cronDO_cmd(CronDO,install_type):
     mkdir_cmd='su - '+cron_acct+' -c "mkdir -p '+cron_zip_home+'"'
     wget_cmd='su - '+cron_acct+' -c "wget '+cron_zip_remote+' -O '+cron_zip_local+'"'
     unzip_cmd='su - '+cron_acct+' -c "cd '+cron_zip_home+' && unzip -o '+cron_zip_name+'"'
-    chmod_cmd='su - '+cron_acct+' -c "find '+cron_mainshell_home+' -name "*.sh" -print0|xargs -0 chmod +x"'
-    dos2unix_cmd='su - '+cron_acct+' -c "find '+cron_mainshell_home+' -type f -regex  ".*.[sh|sql]" -print0| xargs -0 dos2unix  "'
+    chmod_cmd='su - '+cron_acct+' -c "find '+cron_mainshell_home+' -name \'*.sh\' -print0|xargs -0 chmod +x"'
+    dos2unix_cmd='su - '+cron_acct+' -c "find '+cron_mainshell_home+' -type f -regex \'.*\.\(sh\|sql\)\' -print0| xargs -0 dos2unix  "'
     make_logdir_cmd='su - '+cron_acct+' -c " [ ! -d '+cron_log_dir+' ] && mkdir -p '+cron_log_dir+'|| echo log directory already exits "'
 
     zip_install_cmds=(mkdir_cmd,wget_cmd,unzip_cmd,chmod_cmd,dos2unix_cmd,make_logdir_cmd)
